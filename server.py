0abc24d1d7beb7efe2edd594cd935ebc54689813
Julie Norris <julienorris0715@gmail.com>
2020-01-22 16:55:23 -0800

    server working

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from Flask import Flask, request, render_template, session, redirect 
+from flask import Flask, request, render_template, session, redirect 
 import requests, json
 
 app=Flask(__name__)
@@ -14,9 +14,9 @@ When someone visits the site you create, they will be presented with an address 
 
 @app.route('/')
 def index():
-    """Homepage"""
+	"""Homepage"""
 
-    return render_template("address_form.html")
+	return render_template("address_form.html")
 
 
 
@@ -24,41 +24,36 @@ def index():
 
 
 @app.route('/search', methods=['POST'])
-def find_user_OCDID():
-    """Translates the address submitted into OCD-IDs"""
+def get_address():
+	"""Translates the address submitted into OCD-IDs"""
 
-    """setting information submitted from user to variable names"""
-    street = request.form["street"]
-    street2 = request.form["street-2"]
-    city = request.form["city"]
-    state = request.form["state"]
-    zipcode = request.form["zip"]
-    
-    """verifying that the zip code is a 5 or 9 digit numeric entry"""
-    match = re.match(r'^[0-9 -]{5,9}$', zipcode)
-
-    if not match:
-        flash("Invalid zipcode. Try again.")
-        return redirect("/search")
-
-
-def find_upcoming_elections(city, state):
-     """translate the address into some OCD-IDs"""
-
-    ocd-division-state="ocd-division/country:us/state:"+ state.lower()
-    ocd-division-place= ocd-division-state + "/place:" + city.lower()
+	"""setting information submitted from user to variable names"""
+	street = request.form["street"]
+	street2 = request.form["street-2"]
+	city = request.form["city"]
+	state = request.form["state"]
+	zipcode = request.form["zip"]
+	
+	
+	"""translate the address into some OCD-IDs"""
+	state=state.lower()
+	city=city.lower()
+	ocdState="ocd-division/country:us/state:"+ state
+	ocdPlace= ocdState + "/place:" + city
 
 
-    """Query the Democracy Works Elections API for upcoming elections for those OCD-IDs"""
-    payload={'district-divisions':[ocd-division-state, ocd-division-place]}
-    headers = {"Content-Type": "application/json"}
-    url = "https://api.turbovote.org/elections/upcoming?"
-    response = requests.get(url, params=payload, headers=headers)
-    r=response.json()
-    
+	"""Query the Democracy Works Elections API for upcoming elections for those OCD-IDs"""
+	payload={'district-divisions':[ocdState, ocdPlace]}
+	headers = {"Content-Type": "application/json"}
+	url = "https://api.turbovote.org/elections/upcoming?"
+	r = requests.get(url, params=payload, headers=headers)
+	#r=response.json()
+	
+	return (r)
 
-"""Returned elections are displayed to the user
-The results are formatted for the user"""
+
+	"""Returned elections are displayed to the user
+	The results are formatted for the user"""
 @app.route('/election_results', methods=['GET'])
 def display_upcoming_elections():
 
