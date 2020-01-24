7616ccc55c14f7385dc9c884bd1c06746bf5f007
julie-norris <julienorris0715@gmail.com>
2020-01-23 21:31:46 -0800

    added links

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template, session, redirect 
 import requests, json
 from us_states import postal_abbreviations
+from datetime import datetime
 
 
 
 app=Flask(__name__)
-
 
 
 @app.route('/')
@@ -14,25 +14,23 @@ def index():
 
 	return render_template("address_form.html", states=postal_abbreviations)
 
-
-
-"""When the user submits the form, your code will translate the address into some OCD-IDs"""
+"""When the user submits the form the address get translated into some OCD-IDs"""
 
 
 @app.route('/search', methods=['POST'])
 def translate_user_address():
 	"""Translates the address submitted into OCD-IDs"""
 
-	"""setting information submitted from user to variable names"""
+	"""set information submitted from user to variable names"""
 	city = request.form["city"]
 	state = request.form["state"]
 	
 	
-	"""translate the address into some OCD-IDs"""
-	state=state.lower()
-	city=city.lower()
-	ocdState="ocd-division/country:us/state:"+ state
-	ocdPlace= ocdState + "/place:" + city
+	"""translate the address into OCD-IDs"""
+	state = state.lower()
+	city = city.lower()
+	ocdState = "ocd-division/country:us/state:"+ state
+	ocdPlace = ocdState + "/place:" + city
 	
 
 	"""Query the Democracy Works Elections API for upcoming elections for those OCD-IDs"""
@@ -42,6 +40,8 @@ def translate_user_address():
 	
 	headers = {"Accept": "application/json"}
 	url = 'https://api.turbovote.org/elections/upcoming?'
+
+	"""using the requests module the OCD-IDs are added to the URL as the query string and the request is made on the API"""
 	r = requests.get(url, params=params, headers=headers)
 	r=r.text
 	if r == None:
@@ -49,8 +49,7 @@ def translate_user_address():
 	else:
 		rslt=json.loads(r)
 
-	"""Returned elections are displayed to the user
-	The results are formatted for the user"""
+	"""Display upcoming elections returned from API call"""
 
 	return render_template("election_results.html", rslt=rslt)
 
